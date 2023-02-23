ab9e4eb33ed8765c7bb5c73daec67a5925727eb2
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-23 19:56:10 +0530

    Welcome to commercial data processing

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CommercialDataProcessing
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Commercial data processing problem");
+        }
+
+    }
+}
